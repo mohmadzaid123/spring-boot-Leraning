55155a6cbbd0f0139da34aa8269de459d5504872
t-mohammad.zaid <1221265@student.birzeit.edu>
2026-03-12 15:33:32 +0200

    Initial commit

---
+++ src/main/java/org/exalt/springbootleraning/SpringBootLeraningApplication.java
@@ -0,0 +1,13 @@
+package org.exalt.springbootleraning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLeraningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLeraningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/exalt/springbootleraning/SpringBootLeraningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exalt.springbootleraning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLeraningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
